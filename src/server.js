0e3476cccf73aa1891f6db0d650633cf9791d60d
mateusnobre <mateusnobrests@gmail.com>
2021-06-19 04:23:18 -0300

    get categories

--- src/server.js
+++ src/server.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import cors from 'cors';
 import Joi from 'joi';
+import pg from 'pg';
+
+const { Pool } = pg;
+
+const user = 'postgres';
+const password = 'h4ck3rismo';
+const host = 'localhost';
+const port = 5432;
+const database = 'boardcamp';
+
+const connection = new Pool({
+  user,
+  password,
+  host,
+  port,
+  database
+});
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const messageSchema = Joi.object({
-    from: Joi.string().min(1),
-    to: Joi.string().min(1),
-    text: Joi.string().min(1),
-    type: Joi.string().min(1),
-    time: Joi.string().min(1)
+const categorySchema = Joi.object({
+    id: Joi.number().integer(),
+    name: Joi.string().min(1),
+})
+
+app.get("/categories", (req, res) => {
+    const query = connection.query('SELECT * FROM categories');
+    query.then(result => {
+        console.log(result.rows);
+        res.status(200).send(result.rows);
+    })
 })
 
 app.listen(process.env.PORT || 4000)
